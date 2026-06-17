4ec336d8358c1f18f61a3e7e9b037acbec8aac83
4BMtolobPL <108853516+4BMtolobPL@users.noreply.github.com>
2026-06-17 15:59:58 +0900

    kimtalmo-kommit 0.1.1

--- Formula/kimtalmo-kommit.rb
+++ Formula/kimtalmo-kommit.rb
@@ -1,8 +1,8 @@
 class KimtalmoKommit < Formula
   desc "CLI tool using AI to generate Conventional Commits from Git diffs"
   homepage "https://github.com/4BMtolobPL/kommit"
-  url "https://github.com/4BMtolobPL/kommit/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2dda078bc3324213699fd333579be1b90d5217bdde174793495cf6a326856167"
+  url "https://github.com/4BMtolobPL/kommit/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "56fe2b332f32c7a289d4ce65a7682e0192774b9d3dd00760b5c5ba6157807429"
   license "Apache-2.0"
 
   depends_on "rust" => :build
